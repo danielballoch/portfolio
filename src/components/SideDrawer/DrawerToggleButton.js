90d134af5981593690e9182b27d69a766c4ff4d2
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2018-12-16 09:59:28 +1300

    added profile tem, changed mobile menu, started hamburber animation

--- src/components/SideDrawer/DrawerToggleButton.js
+++ src/components/SideDrawer/DrawerToggleButton.js
@@ -1,13 +1,37 @@
 import React from 'react';
-
 import './DrawerToggleButton.css';
 
-const drawerToggleButton = props => (
-    <button className="toggle-button" onClick={props.click}>
-        <div className="toggle-button__line"/>
-        <div className="toggle-button__line"/>
-        <div className="toggle-button__line"/>
-    </button>
-);
+// const drawerToggleButton = props => {
+//     let toggleClasses = 'toggle-button';
+//     if(props.show){
+//         toggleClasses = 'toggle-button open'
+//     }
+
+// return(
+//     <button className={toggleClasses} onClick={props.click}>
+//         <div className="toggle-button__line"/>
+//         <div className="toggle-button__line"/>
+//         <div className="toggle-button__line"/>
+//     </button>
+//     );
+// }
+
+
+class drawerToggleButton extends React.Component{
+constructor(props){
+    super(props);
+}
+
+    render(){
+        return (
+            <button className="toggle-button" onClick={this.props.click}>
+                <div className="toggle-button__line"/>
+                <div className="toggle-button__line"/>
+                <div className="toggle-button__line"/>
+             </button>
+        );
+    }
+}
 
 export default drawerToggleButton;
+
